bd007ddb67570504fd40e90d64909afefec23bff
Arnold Sztandarski <33811566+GeronimoJr@users.noreply.github.com>
2025-05-13 13:35:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ def clean_invalid_xml_chars(text):
     return ''.join(
         c for c in text
         if c in ('\t', '\n', '\r') or
-           (0x20 <= ord(c) <= 0xD7FF) or
-           (0xE000 <= ord(c) <= 0xFFFD) or
-           (0x10000 <= ord(c) <= 0x10FFFF)
+        (0x20 <= ord(c) <= 0xD7FF) or
+        (0xE000 <= ord(c) <= 0xFFFD) or
+        (0x10000 <= ord(c) <= 0x10FFFF)
 )
 
 
